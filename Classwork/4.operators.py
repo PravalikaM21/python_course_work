a0af6039ddf7f4bf19636ae1c9b39e616026338d
PravalikaM21 <muchanthalapravalika@gmail.com>
2025-07-02 11:25:38 +0530

    02/07/2025

---
+++ Classwork/4.operators.py
@@ -0,0 +1,11 @@
+#4.Python Operators
+#1.Arithmetic operators
+a=20
+b=10
+print("Addition(+):",(a+b)) #Addition(+): 30
+print("Subtraction(-):",(a-b)) # Subtraction(-): 10
+print("Multiplication(+):",(a*b)) #Multiplication(+): 200
+print("Division(/):",(a/b)) #Division(/): 2.0
+print("Floor Division(//):",(a//b)) #Floor Division(//): 2
+print("Modulus(%):",(a%b)) #Modulus(%): 0
+print("Exponentiation(**):",(a**b)) #Exponentiation(**): 10240000000000
