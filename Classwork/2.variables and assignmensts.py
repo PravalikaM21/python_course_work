d4a568f5050e8846a6fc9db4824cb6b30af5c313
PravalikaM21 <muchanthalapravalika@gmail.com>
2025-07-02 22:24:28 +0530

    28/06/2025

---
+++ Classwork/2.variables and assignmensts.py
@@ -0,0 +1,32 @@
+#2.variables.py
+#Must start with a letter(A-Z,a-z) or underscore
+#can not start with a number
+#can contain a letter 
+#can contain a digits
+#can contain a underscore
+#can not start with a number
+#cant contain special characters
+# can not be keyword
+# python is case sensitive
+number=10 
+print(number)#10
+NAME = "Prava"
+print(NAME)  #Prava
+Value="R" 
+print(Value) # R
+_num = 30 
+print(_num) #30
+num5 = 10 
+print(num5) # 10
+'''
+5name = "sai" 
+print(5num) #SyntaxError: invalid decimal literal
+$num = 50 
+print($num) #SyntaxError: invalid syntax
+True = 1 
+print(True) #SyntaxError: cannot assign to True
+course = "Ds" 
+print(course)  #Ds
+COURSE = "Da" 
+print(COURSE) #Da
+'''
