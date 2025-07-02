f7e33028d1b7d6077535c0e43fefbd97af004cd2
PravalikaM21 <muchanthalapravalika@gmail.com>
2025-07-02 22:27:08 +0530

    28/06/2025

--- Classwork/2.variables and assignmensts.py
+++ Classwork/2.variables and assignmensts.py
@@ -9,15 +9,15 @@
 # can not be keyword
 # python is case sensitive
 number=10 
-print(number)#10
+print(number)#10(valid)
 NAME = "Prava"
-print(NAME)  #Prava
+print(NAME)  #Prava(valid)
 Value="R" 
-print(Value) # R
+print(Value) # R(valid)
 _num = 30 
-print(_num) #30
+print(_num) #30(valid)
 num5 = 10 
-print(num5) # 10
+print(num5) # 10(valid)
 '''
 5name = "sai" 
 print(5num) #SyntaxError: invalid decimal literal
@@ -26,7 +26,7 @@ print($num) #SyntaxError: invalid syntax
 True = 1 
 print(True) #SyntaxError: cannot assign to True
 course = "Ds" 
-print(course)  #Ds
+print(course)  #Ds(valid)
 COURSE = "Da" 
-print(COURSE) #Da
+print(COURSE) #Da(valid)
 '''
