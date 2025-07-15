416c9b98051b50ea26eaf6b6027064d1888c27ad
PravalikaM21 <muchanthalapravalika@gmail.com>
2025-07-15 08:15:58 +0530

    14/07/2025

---
+++ Practice/Practice.py
@@ -0,0 +1,56 @@
+num=int(input())
+if num % 3 ==0:
+  if num>5:
+    print("yes")
+  else:
+    print("no")
+else:
+  print("no")
+#----------------------------------------
+num=int(input())
+if num % 3 ==0 and num > 5:
+    print("yes")
+else:
+  print("no")
+#----------------------------------------
+num=int(input())
+if num >0:
+    if num%2 == 0:
+        print("yes")
+    else:
+        print("no")
+else:
+    print("no")
+#output: 10
+        #yes
+        #7
+        #no
+#----------------------------------------
+num=int(input())
+if num >0:
+    if num%2 == 0:
+        print("yes")
+    else:
+        print("no")
+else:
+    print("no")
+#output: 10
+        #yes
+        #7
+        #no
+#-------------------------------------------------
+lst=[2,3]
+lst.insert(1,1)
+print(lst) #[2, 1, 3]
+
+lst=[1,3]
+lst.insert(0,2)
+print(lst) #[2, 1, 3]
+
+l=[7,8]
+l.append(9)
+print(l)
+
+print(l.append(9))
+
+print([7,8].append(9))
