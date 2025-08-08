cb425e9599a07bb7de3e6adced492e81a8e58c95
PravalikaM21 <pravalikamuchanthala@gmail.com>
2025-08-08 13:41:16 +0530

    08/08/2025

--- Classwork/lambda_fun.py
+++ Classwork/lambda_fun.py
@@ -8,11 +8,26 @@ print(s)
 l=["Pravalika","shailu","Anu"]
 s=sorted(l,key=lambda i:i[-1])
 print(s)
-'''
+
 data={'tarit':90,'prava':98,'dharani':80,'shivani':67}
-sorted_data=sorted(data.items())
+sorted_data=sorted(data.items(),key=lambda i:i[1])
 print(sorted_data)
 print(sorted(data))
 print(data.keys())
 print(sorted(data.values()))
 
+list={1:3,2:1,4:2}
+sorted_list=sorted(list.items(),key=lambda i: i[1])
+print(sorted_list)
+
+l=[1,2,3,4,5,6,7,8,9]
+even=list(map(lambda l:l+2).split())
+print(even)
+'''
+
+l=(lambda i:len(i))("python")
+print(l)
+
+name="python"
+l=(lambda i:len(i))(name)
+print(l)
