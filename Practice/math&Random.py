20c35cf0dbe0403523db7896886a5cf5c60cfcd5
PravalikaM21 <pravalikamuchanthala@gmail.com>
2025-08-20 19:28:17 +0530

    20/08/2025

---
+++ Practice/math&Random.py
@@ -0,0 +1,38 @@
+#1) Find square root a number:
+import math
+import random
+'''num=int(input("enter a number:"))
+print(math.sqrt(num))
+
+num=int(input("enter a number:"))
+print(random.randint(1,10))
+print(math.factorial(5))
+print(random.random())
+
+#1)Add 2 numbers(function)
+def add(a,b):
+    return a+b
+a= int(input("enter a value"))
+b= int(input("enter b valur"))
+print(add(a,b))
+
+#1)Add 2 numbers(lambda)
+a= int(input("enter a value"))
+b= int(input("enter b valur"))
+add=lambda a,b:a+b
+print(add(a,b))
+#(or)
+add=lambda a,b:a+b
+print(add(5,6))
+
+
+#2)square a number:
+def sqr(n):
+    return n**2
+n=int(input("enter a number"))
+print(sqr(n))
+#Area of a circle:'''
+def area_of_circle(radius):
+    return math.pi*radius**2
+r=float(input('enter a avlue'))
+print(area_of_circle(r))
