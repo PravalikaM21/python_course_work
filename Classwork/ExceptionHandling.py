9efacffe7216f777a10d3abbfd5c4ae97f216b81
PravalikaM21 <pravalikamuchanthala@gmail.com>
2025-08-21 12:04:48 +0530

    21/08/2025

--- Classwork/ExceptionHandling.py
+++ Classwork/ExceptionHandling.py
@@ -1,3 +1,4 @@
+# Method 1:
 try:
     a=10
     a=a+10
@@ -18,3 +19,31 @@ except ValueError:
     print("Enter the proper datatype")
 except TypeError:
     print("You cant add 2 diff types")
+#-----------------------------------------
+# Method 2:
+try:
+    a=10
+    a=a+10
+    l=[1,3,4]
+    k=l[2]
+    d={1:2,2:4,3:9}
+    print(d[2])
+    b=int(input("Enter a number:"))
+    print(b)
+    c=1+"1"
+except (NameError,IndexError,KeyError,ValueError,TypeError) as e:
+    print(f"Error occured: {e}")
+#------------------------------------------------------
+# Method 3:
+try:
+    a=10
+    a=a+10
+    l=[1,3,4]
+    k=l[2]
+    d={1:2,2:4,3:9}
+    print(d[2])
+    b=int(input("Enter a number:"))
+    print(b)
+    c=1+"1"
+except Exception as e:
+    print(f"Error occured: {e}")
